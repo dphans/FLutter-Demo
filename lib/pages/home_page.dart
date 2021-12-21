fe8a7477748a8e091e18ac3ed04c9ee43ea332b7
Bao Phan <contact@dphans.me>
2021-12-21 10:34:00 +0700

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,41 +8,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
-  void initState() {
-    super.initState();
-
-    // Code o day
-    // int soA = 5;
-    // int soB = 6;
-    // int ketQua = congHaiSo(soA, soB);
-    // debugPrint("Ket qua $soA va $soB la $ketQua");
-  }
-
   int congHaiSo(int a, int b) => a + b;
 
-  void startCode() {
-    // List
-    List<String> danhSachThanhVien = [];
-
-    danhSachThanhVien.add("Van");
-    danhSachThanhVien.add("Vu");
-    danhSachThanhVien.addAll(["Bao", "Nguyen"]);
-
-    debugPrint("Tong so thanh vien: ${danhSachThanhVien.length}");
-    danhSachThanhVien.forEach((name) {
-      debugPrint(name);
-    });
-
-    List<double> diemSo = [4, 5, 6, 2, 3, 4];
-    double soLonNhat = diemSo[0];
-    diemSo.forEach((diem) {
-      if (diem > soLonNhat) {
-        soLonNhat = diem;
-      }
-    });
-    debugPrint("Diem lon nhat la: $soLonNhat");
-  }
+  void startCode() {}
 
   @override
   Widget build(BuildContext context) {
